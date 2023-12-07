92f24b2d190aaf1639b85c7e0edcd4f2aeecefdf
withoutspaces <dms@ic.ufal.br>
2023-12-07 17:35:23 -0300

    feat: add route

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -9,50 +9,65 @@ import {
   CheckBox,
 } from "./styles";
 import { ScrollView, View } from "react-native";
-import Header from "../../components/Header";
-import GeneralScreenContainer from "../../components/GeneralScreenContainer";
-import Button from "../../components/Button";
-import Footer from "../../components/Footer";
-import TextField from "../../components/TextField";
+import { useNavigation } from "@react-navigation/native";
 import { themes } from "../../global/themes";
 
+import GeneralScreenContainer from "../../components/GeneralScreenContainer";
+import TextField from "../../components/TextField";
+import Header from "../../components/Header";
+import Button from "../../components/Button";
+import Footer from "../../components/Footer";
+
 export default function Login() {
+  const navigation = useNavigation();
+
+  const handleChangeScreen = () => {
+    navigation.navigate("Register" as never);
+  };
+
   return (
     <Container>
       <Header />
       <GeneralScreenContainer>
         <ScrollView>
-
-        <Title>Boas-vindas à Zion!</Title>
-        <View style={{ flexDirection: "row", gap: 8, marginBottom: 40 }}>
-          <Subtitle>Novo por aqui?</Subtitle>
-          <TextLink>
-            <SpecialText>Crie sua conta agora</SpecialText>
-          </TextLink>
-        </View>
-        <View style={{ gap: 24 }}>
-          <TextField label="E-mail" placeholder="bcerqueira94@gmail.com" />
-          <TextField label="Senha" placeholder="•••••••••••••" />
-        </View>
-
-        <View
-          style={{ marginTop: 24, flexDirection: "row", justifyContent: "space-between"}}
-        >
-          <View style={{flexDirection: "row", alignItems: "center", gap: 10}}>
-            <CheckBox />
-            <Text $color={themes.colors.textSecondary}>Manter conectado</Text>
+          <Title>Boas-vindas à Zion!</Title>
+          <View style={{ flexDirection: "row", gap: 8, marginBottom: 40 }}>
+            <Subtitle>Novo por aqui?</Subtitle>
+            <TextLink onPress={handleChangeScreen}>
+              <SpecialText>Crie sua conta agora</SpecialText>
+            </TextLink>
           </View>
-          <TextLink>
-            <SpecialText>Esqueci minha senha</SpecialText>
-          </TextLink>
-        </View>
 
-        <Button>
-          <Text $size={16} $weight="bold">
-            Entrar
-          </Text>
-        </Button>
-        <Footer />
+          {/* Form */}
+          <View style={{ gap: 24 }}>
+            <TextField label="E-mail" placeholder="bcerqueira94@gmail.com" />
+            <TextField label="Senha" placeholder="•••••••••••••" />
+          </View>
+
+          <View
+            style={{
+              marginTop: 24,
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <View
+              style={{ flexDirection: "row", alignItems: "center", gap: 10 }}
+            >
+              <CheckBox />
+              <Text $color={themes.colors.textSecondary}>Manter conectado</Text>
+            </View>
+            <TextLink>
+              <SpecialText>Esqueci minha senha</SpecialText>
+            </TextLink>
+          </View>
+
+          <Button>
+            <Text $size={16} $weight="bold">
+              Entrar
+            </Text>
+          </Button>
+          <Footer />
         </ScrollView>
       </GeneralScreenContainer>
     </Container>
